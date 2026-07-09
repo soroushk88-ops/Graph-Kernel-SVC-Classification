fa595d6c0486d5ab585b54f2a1d0499fa62bcc14
Soroush Karami <soroushk88@gmail.com>
2026-07-09 17:22:41 +0200

    Add files via upload

---
+++ MUTAG_classification.py
@@ -0,0 +1,142 @@
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn.manifold import Isomap, SpectralEmbedding, LocallyLinearEmbedding
+
+from grakel.kernels import ShortestPath, GraphletSampling, RandomWalk, WeisfeilerLehman
+from grakel.datasets.base import read_data
+import gdown
+import warnings
+warnings.filterwarnings('ignore')
+
+MUTAG = read_data("MUTAG")
+graph_data, y = np.array(MUTAG.data), np.array(MUTAG.target)
+G_train, G_test, y_train, y_test = train_test_split(graph_data, y, test_size=0.1, random_state=42)
+
+# Shortest Path kernel
+g_kernel = ShortestPath(normalize = True)
+K_train = g_kernel.fit_transform(G_train)
+K_test = g_kernel.transform(G_test)
+model = SVC()
+model.fit(K_train, y_train)
+y_pred = model.predict(K_test)
+
+acc = accuracy_score(y_test, y_pred)
+prec = precision_score(y_test, y_pred)
+rec = recall_score(y_test, y_pred)
+print(f"MUTAG dataset: Shortest Path kernel ===> acuuracy: {round(acc, 4)}, precision: {round(prec, 4)}, recall: {round(rec, 4)}")
+
+manifolds_names = ['Isomap', 'LocallyLinearEmbedding', 'SpectralEmbedding']
+manifolds = [Isomap, LocallyLinearEmbedding, SpectralEmbedding]
+
+for j, manif in enumerate(manifolds):
+    encoder = manif(n_components = 10)
+    E_train = encoder.fit_transform(K_train)
+    E_test = encoder.transform(K_test) if j < 2 else encoder.fit_transform(K_test)
+
+    model = SVC()
+    model.fit(E_train, y_train)
+    y_pred = model.predict(E_test)
+
+    acc = accuracy_score(y_test, y_pred)
+    prec = precision_score(y_test, y_pred)
+    rec = recall_score(y_test, y_pred)
+    print(f"MUTAG dataset: Shortest Path kernel + {manifolds_names[j]} ===> acuuracy: {round(acc, 4)}, precision: {round(prec, 4)}, recall: {round(rec, 4)}")
+
+
+
+# Graphlet Sampling kernel
+g_kernel = GraphletSampling(normalize = True)
+K_train = g_kernel.fit_transform(G_train)
+K_test = g_kernel.transform(G_test)
+model = SVC()
+model.fit(K_train, y_train)
+y_pred = model.predict(K_test)
+
+acc = accuracy_score(y_test, y_pred)
+prec = precision_score(y_test, y_pred)
+rec = recall_score(y_test, y_pred)
+print(f"MUTAG dataset: Graphlet Sampling kernel ===> acuuracy: {round(acc, 4)}, precision: {round(prec, 4)}, recall: {round(rec, 4)}")
+
+manifolds_names = ['Isomap', 'LocallyLinearEmbedding', 'SpectralEmbedding']
+manifolds = [Isomap, LocallyLinearEmbedding, SpectralEmbedding]
+
+for j, manif in enumerate(manifolds):
+    encoder = manif(n_components = 10)
+    E_train = encoder.fit_transform(K_train)
+    E_test = encoder.transform(K_test) if j < 2 else encoder.fit_transform(K_test)
+
+    model = SVC()
+    model.fit(E_train, y_train)
+    y_pred = model.predict(E_test)
+
+    acc = accuracy_score(y_test, y_pred)
+    prec = precision_score(y_test, y_pred)
+    rec = recall_score(y_test, y_pred)
+    print(f"MUTAG dataset: Graphlet Sampling kernel + {manifolds_names[j]} ===> acuuracy: {round(acc, 4)}, precision: {round(prec, 4)}, recall: {round(rec, 4)}")
+
+
+
+# Random Walk kernel
+g_kernel = RandomWalk(normalize = True)
+K_train = g_kernel.fit_transform(G_train)
+K_test = g_kernel.transform(G_test)
+model = SVC()
+model.fit(K_train, y_train)
+y_pred = model.predict(K_test)
+
+acc = accuracy_score(y_test, y_pred)
+prec = precision_score(y_test, y_pred)
+rec = recall_score(y_test, y_pred)
+print(f"MUTAG dataset: Random Walk kernel ===> acuuracy: {round(acc, 4)}, precision: {round(prec, 4)}, recall: {round(rec, 4)}")
+
+manifolds_names = ['Isomap', 'LocallyLinearEmbedding', 'SpectralEmbedding']
+manifolds = [Isomap, LocallyLinearEmbedding, SpectralEmbedding]
+
+for j, manif in enumerate(manifolds):
+    encoder = manif(n_components = 10)
+    E_train = encoder.fit_transform(K_train)
+    E_test = encoder.transform(K_test) if j < 2 else encoder.fit_transform(K_test)
+
+    model = SVC()
+    model.fit(E_train, y_train)
+    y_pred = model.predict(E_test)
+
+    acc = accuracy_score(y_test, y_pred)
+    prec = precision_score(y_test, y_pred)
+    rec = recall_score(y_test, y_pred)
+    print(f"MUTAG dataset: Random Walk kernel + {manifolds_names[j]} ===> acuuracy: {round(acc, 4)}, precision: {round(prec, 4)}, recall: {round(rec, 4)}")
+
+
+
+# Weisfeiler-Lehman kernel
+g_kernel = WeisfeilerLehman(normalize = True)
+K_train = g_kernel.fit_transform(G_train)
+K_test = g_kernel.transform(G_test)
+model = SVC()
+model.fit(K_train, y_train)
+y_pred = model.predict(K_test)
+
+acc = accuracy_score(y_test, y_pred)
+prec = precision_score(y_test, y_pred)
+rec = recall_score(y_test, y_pred)
+print(f"MUTAG dataset: Weisfeiler-Lehman kernel ===> acuuracy: {round(acc, 4)}, precision: {round(prec, 4)}, recall: {round(rec, 4)}")
+
+manifolds_names = ['Isomap', 'LocallyLinearEmbedding', 'SpectralEmbedding']
+manifolds = [Isomap, LocallyLinearEmbedding, SpectralEmbedding]
+
+for j, manif in enumerate(manifolds):
+    encoder = manif(n_components = 10)
+    E_train = encoder.fit_transform(K_train)
+    E_test = encoder.transform(K_test) if j < 2 else encoder.fit_transform(K_test)
+
+    model = SVC()
+    model.fit(E_train, y_train)
+    y_pred = model.predict(E_test)
+
+    acc = accuracy_score(y_test, y_pred)
+    prec = precision_score(y_test, y_pred)
+    rec = recall_score(y_test, y_pred)
+    print(f"MUTAG dataset: Weisfeiler-Lehman kernel + {manifolds_names[j]} ===> acuuracy: {round(acc, 4)}, precision: {round(prec, 4)}, recall: {round(rec, 4)}")
---
+++ PTC_classification.py
@@ -0,0 +1,144 @@
+# PTC data
+
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn.manifold import Isomap, SpectralEmbedding, LocallyLinearEmbedding
+
+from grakel.kernels import ShortestPath, GraphletSampling, RandomWalk, WeisfeilerLehman
+from grakel.datasets.base import read_data
+import gdown
+import warnings
+warnings.filterwarnings('ignore')
+
+PTC = read_data("PTC_MR")
+graph_data, y = np.array(PTC.data), np.array(PTC.target)
+G_train, G_test, y_train, y_test = train_test_split(graph_data, y, test_size=0.1, random_state=42)
+
+# Shortest Path kernel
+g_kernel = ShortestPath(normalize = True)
+K_train = g_kernel.fit_transform(G_train)
+K_test = g_kernel.transform(G_test)
+model = SVC()
+model.fit(K_train, y_train)
+y_pred = model.predict(K_test)
+
+acc = accuracy_score(y_test, y_pred)
+prec = precision_score(y_test, y_pred)
+rec = recall_score(y_test, y_pred)
+print(f"PTC dataset: Shortest Path kernel ===> acuuracy: {round(acc, 4)}, precision: {round(prec, 4)}, recall: {round(rec, 4)}")
+
+manifolds_names = ['Isomap', 'LocallyLinearEmbedding', 'SpectralEmbedding']
+manifolds = [Isomap, LocallyLinearEmbedding, SpectralEmbedding]
+
+for j, manif in enumerate(manifolds):
+    encoder = manif(n_components = 10)
+    E_train = encoder.fit_transform(K_train)
+    E_test = encoder.transform(K_test) if j < 2 else encoder.fit_transform(K_test)
+
+    model = SVC()
+    model.fit(E_train, y_train)
+    y_pred = model.predict(E_test)
+
+    acc = accuracy_score(y_test, y_pred)
+    prec = precision_score(y_test, y_pred)
+    rec = recall_score(y_test, y_pred)
+    print(f"PTC dataset: Shortest Path kernel + {manifolds_names[j]} ===> acuuracy: {round(acc, 4)}, precision: {round(prec, 4)}, recall: {round(rec, 4)}")
+
+
+
+# Graphlet Sampling kernel
+g_kernel = GraphletSampling()
+K_train = g_kernel.fit_transform(G_train)
+K_test = g_kernel.transform(G_test)
+model = SVC()
+model.fit(K_train, y_train)
+y_pred = model.predict(K_test)
+
+acc = accuracy_score(y_test, y_pred)
+prec = precision_score(y_test, y_pred)
+rec = recall_score(y_test, y_pred)
+print(f"PTC dataset: Graphlet Sampling kernel ===> acuuracy: {round(acc, 4)}, precision: {round(prec, 4)}, recall: {round(rec, 4)}")
+
+manifolds_names = ['Isomap', 'LocallyLinearEmbedding', 'SpectralEmbedding']
+manifolds = [Isomap, LocallyLinearEmbedding, SpectralEmbedding]
+
+for j, manif in enumerate(manifolds):
+    encoder = manif(n_components = 10)
+    E_train = encoder.fit_transform(K_train)
+    E_test = encoder.transform(K_test) if j < 2 else encoder.fit_transform(K_test)
+
+    model = SVC()
+    model.fit(E_train, y_train)
+    y_pred = model.predict(E_test)
+
+    acc = accuracy_score(y_test, y_pred)
+    prec = precision_score(y_test, y_pred)
+    rec = recall_score(y_test, y_pred)
+    print(f"PTC dataset: Graphlet Sampling kernel + {manifolds_names[j]} ===> acuuracy: {round(acc, 4)}, precision: {round(prec, 4)}, recall: {round(rec, 4)}")
+
+
+
+# Random Walk kernel
+g_kernel = RandomWalk()
+K_train = g_kernel.fit_transform(G_train)
+K_test = g_kernel.transform(G_test)
+model = SVC()
+model.fit(K_train, y_train)
+y_pred = model.predict(K_test)
+
+acc = accuracy_score(y_test, y_pred)
+prec = precision_score(y_test, y_pred)
+rec = recall_score(y_test, y_pred)
+print(f"PTC dataset: Random Walk kernel ===> acuuracy: {round(acc, 4)}, precision: {round(prec, 4)}, recall: {round(rec, 4)}")
+
+manifolds_names = ['Isomap', 'LocallyLinearEmbedding', 'SpectralEmbedding']
+manifolds = [Isomap, LocallyLinearEmbedding, SpectralEmbedding]
+
+for j, manif in enumerate(manifolds):
+    encoder = manif(n_components = 10)
+    E_train = encoder.fit_transform(K_train)
+    E_test = encoder.transform(K_test) if j < 2 else encoder.fit_transform(K_test)
+
+    model = SVC()
+    model.fit(E_train, y_train)
+    y_pred = model.predict(E_test)
+
+    acc = accuracy_score(y_test, y_pred)
+    prec = precision_score(y_test, y_pred)
+    rec = recall_score(y_test, y_pred)
+    print(f"PTC dataset: Random Walk kernel + {manifolds_names[j]} ===> acuuracy: {round(acc, 4)}, precision: {round(prec, 4)}, recall: {round(rec, 4)}")
+
+
+
+# Weisfeiler-Lehman kernel
+g_kernel = WeisfeilerLehman(normalize = True)
+K_train = g_kernel.fit_transform(G_train)
+K_test = g_kernel.transform(G_test)
+model = SVC()
+model.fit(K_train, y_train)
+y_pred = model.predict(K_test)
+
+acc = accuracy_score(y_test, y_pred)
+prec = precision_score(y_test, y_pred)
+rec = recall_score(y_test, y_pred)
+print(f"PTC dataset: Weisfeiler-Lehman kernel ===> acuuracy: {round(acc, 4)}, precision: {round(prec, 4)}, recall: {round(rec, 4)}")
+
+manifolds_names = ['Isomap', 'LocallyLinearEmbedding', 'SpectralEmbedding']
+manifolds = [Isomap, LocallyLinearEmbedding, SpectralEmbedding]
+
+for j, manif in enumerate(manifolds):
+    encoder = manif(n_components = 10)
+    E_train = encoder.fit_transform(K_train)
+    E_test = encoder.transform(K_test) if j < 2 else encoder.fit_transform(K_test)
+
+    model = SVC()
+    model.fit(E_train, y_train)
+    y_pred = model.predict(E_test)
+
+    acc = accuracy_score(y_test, y_pred)
+    prec = precision_score(y_test, y_pred)
+    rec = recall_score(y_test, y_pred)
+    print(f"PTC dataset: Weisfeiler-Lehman kernel + {manifolds_names[j]} ===> acuuracy: {round(acc, 4)}, precision: {round(prec, 4)}, recall: {round(rec, 4)}")
